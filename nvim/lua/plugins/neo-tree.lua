7afbd99a6493045542022f35ffa70aa5190283c7
kieren hussey <khussey45@gmail.com>
2025-08-30 18:15:16 -0500

    Neo tree fix

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -8,6 +8,7 @@
       "MunifTanjim/nui.nvim",
     },
     config = function()
-      vim.keymap.set('n','<C-n>', ':Neotree filesystem reveal left<CR>', {})
+      vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", { silent = true })
+
     end
   }
